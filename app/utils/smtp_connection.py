f6e29516ae1a1f58da77f646b75bb7680023ead0
Sarachaker <sc2444@njit.edu>
2025-04-24 06:17:35 +0500

    Update smtp_connection.py

--- app/utils/smtp_connection.py
+++ app/utils/smtp_connection.py
@@ -21,10 +21,10 @@ class SMTPClient:
             message['To'] = recipient
             message.attach(MIMEText(html_content, 'html'))
 
-            with smtplib.SMTP(self.server, self.port) as server:
-                server.starttls()  # Use TLS
-                server.login(self.username, self.password)
-                server.sendmail(self.username, recipient, message.as_string())
+            with smtplib.SMTP(self.server, self.port) as stmp:
+                stmp.starttls()  # Use TLS
+                stmp.login(self.username, self.password)
+                stmp.sendmail(self.username, recipient, message.as_string())
             logging.info(f"Email sent to {recipient}")
         except Exception as e:
             logging.error(f"Failed to send email: {str(e)}")
